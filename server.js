696bcc11eb96adfe23a046211231ad6fb00dc3ac
Louis Jullien <louis.jullien80@gmail.com>
2020-12-08 15:47:58 +0100

    Ajout dans server.js et bootstrap menu

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ let express = require ('express');
 let app = express();
 let port = 3000;
 
+//Préparation du serveur
+app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use(express.static(__dirname + 'node_modules/jquery/dist'));
+app.use(express.static(__dirname + 'node_modules/popper.js/dist/umd'));
+app.use(express.static(__dirname + 'node_modules/bootstrap/dist/css'));
 app.listen(port, () => {
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
 })
 
+//Routes
 app.get('/', (req, res, next) => {
-    res.send('Bonjour lé kop1!');
+    res.sendFile('www/index.html');
 });
